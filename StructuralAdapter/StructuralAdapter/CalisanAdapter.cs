cb83471bd8c592e8532ea1b26a2492faf9edb15a
bayzinbarkin <barkin.bayzin@bilgeadamboost.com>
2022-10-15 13:03:23 +0300

    new dp added

---
+++ StructuralAdapter/StructuralAdapter/CalisanAdapter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructuralAdapter
+{
+    public class CalisanAdapter : CalisanSistem, ITarget
+    {
+        public List<string> GetEmployeeList()
+        {
+            List<string> calisanList = new List<string>();
+            string[][] employees = CalisanGetir();
+            foreach (string[] employee in employees)
+            {
+                calisanList.Add(employee[0]);
+                calisanList.Add(",");
+                calisanList.Add(employee[1]);
+                calisanList.Add(",");
+                calisanList.Add(employee[2]);
+                calisanList.Add("\n");
+            }
+
+            return calisanList;
+        }
+    }
+}
